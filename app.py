ebd9f0bb77ea6e04aa4df9279e7dfd86289eda05
rakshith-ss <ghostingo@protonmail.com>
2020-10-12 19:02:09 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import MakePost
+from get_jokes import fetch_joke
 from datetime import datetime
 
 app = Flask(__name__)
@@ -23,7 +24,12 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post( '{self.title}' , '{self.author}' , '{self.date_posted}' )"
 
+class Joke(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    joke_ = db.Column(db.Text, nullable = False)
 
+    def __repr__(self):
+        return f" {self.joke_}"
 
 # this route(address) to the home page
 # return render_template looks for home.html
@@ -50,6 +56,22 @@ def posts():
         db.session.commit()
         return redirect('/home')
     return render_template('post.html', post=post)
+
+
+
+dadjoke = fetch_joke()
+for joke in dadjoke:
+    tell_joke = Joke(joke_ = joke)
+    db.session.add(tell_joke)
+    db.session.commit()
+
+
+
+@app.route('/jokes')
+def jokes():
+    blogjokes = Joke.query.all()
+    return render_template('joke.html', blogjokes = blogjokes)
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
