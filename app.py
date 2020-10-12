bdfd9125b2c4072e0ed55bd08ba6c52dccfa6de9
rakshith-ss <ghostingo@protonmail.com>
2020-10-12 13:39:22 +0530

    added comments to app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, url_for, render_template, request, url_for, request, redirect
+from flask import Flask, url_for, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from forms import LoginForm, MakePost
+from forms import MakePost
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "You Found out dude"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///write.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///write.db' #will create a database in the current folder
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False         # don't track modifications because it will use a lot of resource
 
 # create a database instance 
 db = SQLAlchemy(app)
 
+# a  database model(object) to create a post
 class Post(db.Model):
     id = db.Column(db.Integer , primary_key = True)
     # create a column for time stamp
@@ -22,22 +23,28 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post( '{self.title}' , '{self.author}' , '{self.date_posted}' )"
 
+
+
+# this route(address) to the home page
+# return render_template looks for home.html
+# and returns the home.html page
 @app.route('/home')
-def index():
+def home():
     blogposts = Post.query.all() # fetch records table
     return render_template('home.html', blogposts = blogposts)
 
-
+# route to the about page 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
 
+# create post 
 @app.route('/post', methods=['GET','Post'])
 def posts():
     post = MakePost()
-    if post.is_submitted():
+    if post.validate_on_submit():
         blogpost = Post(title = post.title.data, author = post.author.data , content = post.message.data )
         db.session.add(blogpost)
         db.session.commit()
